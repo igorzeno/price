e13d08ca515addb8da2507b10572f7fe862c1bbb
IgorStarozhuckov <starozhuckov@yandex.ru>
2025-07-03 10:46:55 +0300

    Init commit

--- app/Http/Services/CurrencyConvertService.php
+++ app/Http/Services/CurrencyConvertService.php
@@ -12,7 +12,7 @@ class CurrencyConvertService
     {
         return match (request('currency')) {
             self::RUB => number_format($price * env('RUB', 100), 0, '.', ' ') .' ₽',
-            self::USD => '$'. number_format((float)$price * env('USD', 0.9), 2, '.',''),
+            self::USD => '$'. number_format((float)$price * env('USD', 1.17), 2, '.',''),
             self::EUR => '€'. number_format((float)$price , 2, '.',''),
             default => 'Not Found',
         };
